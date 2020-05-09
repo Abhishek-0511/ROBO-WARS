0eb641bae3496049f4231eb78db00925689eaf34
Abhishek-0511 <57386129+Abhishek-0511@users.noreply.github.com>
2020-05-09 15:38:23 +0530

    Final Game

---
+++ Assets/Scripts/Enemy Scripts/EnemyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyManager : MonoBehaviour
+{
+    private EnemyScript[] enemies;
+    void Start()
+    {
+        enemies = GameObject.FindObjectsOfType<EnemyScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemies.Length<1)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/UIMANAGER.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMANAGER : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
